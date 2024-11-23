87012f2d1a564751a6a8ead88e55aae5e95996a1
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-11-23 15:55:57 +0530

    define buttons.

--- app/src/main/java/com/example/calculatormobileapp/MainActivity.java
+++ app/src/main/java/com/example/calculatormobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculatormobileapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button num0 = findViewById(R.id.num0);
+        Button num1 = findViewById(R.id.num1);
+        Button num2 = findViewById(R.id.num2);
+        Button num3 = findViewById(R.id.num3);
+        Button num4 = findViewById(R.id.num4);
+        Button num5 = findViewById(R.id.num5);
+        Button num6 = findViewById(R.id.num6);
+        Button num7 = findViewById(R.id.num7);
+        Button num8 = findViewById(R.id.num8);
+        Button num9 = findViewById(R.id.num9);
+
+        Button on = findViewById(R.id.on);
+        Button off = findViewById(R.id.off);
+        Button ac = findViewById(R.id.ac);
+        Button del = findViewById(R.id.del);
+        Button period = findViewById(R.id.period);
+        Button add = findViewById(R.id.add);
+        Button sub = findViewById(R.id.sub);
+        Button mul = findViewById(R.id.mul);
+        Button div = findViewById(R.id.div);
+        Button equal = findViewById(R.id.equal);
     }
 }
