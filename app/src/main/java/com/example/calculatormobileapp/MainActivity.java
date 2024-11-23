a86443de84caabee91decbc3111365d9542ae52a
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-11-23 20:23:17 +0530

    bug fixes and code optimization

--- app/src/main/java/com/example/calculatormobileapp/MainActivity.java
+++ app/src/main/java/com/example/calculatormobileapp/MainActivity.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     double firstNum;
     String operation;
+    boolean isOperatorClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +29,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button num0 = findViewById(R.id.num0);
-        Button num1 = findViewById(R.id.num1);
-        Button num2 = findViewById(R.id.num2);
-        Button num3 = findViewById(R.id.num3);
-        Button num4 = findViewById(R.id.num4);
-        Button num5 = findViewById(R.id.num5);
-        Button num6 = findViewById(R.id.num6);
-        Button num7 = findViewById(R.id.num7);
-        Button num8 = findViewById(R.id.num8);
-        Button num9 = findViewById(R.id.num9);
-
         Button on = findViewById(R.id.on);
         Button off = findViewById(R.id.off);
         Button ac = findViewById(R.id.ac);
         Button del = findViewById(R.id.del);
         Button period = findViewById(R.id.period);
-        Button add = findViewById(R.id.add);
-        Button sub = findViewById(R.id.sub);
-        Button mul = findViewById(R.id.mul);
-        Button div = findViewById(R.id.div);
-        Button equal = findViewById(R.id.equal);
 
         TextView screen = findViewById(R.id.screen);
+        Button equal = findViewById(R.id.equal);
 
         ac.setOnClickListener(view -> {
             firstNum = 0;
@@ -78,7 +64,10 @@ public class MainActivity extends AppCompatActivity {
         for (Button b : nums) {
             b.setOnClickListener(view -> {
                 String currentText = screen.getText().toString();
-                if (!currentText.equals("0")) {
+                if (isOperatorClicked) {
+                    screen.setText(b.getText().toString());
+                    isOperatorClicked = false; // Reset after first digit of second number
+                } else if (!currentText.equals("0")) {
                     screen.setText(currentText + b.getText().toString());
                 } else {
                     screen.setText(b.getText().toString());
@@ -97,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     firstNum = Double.parseDouble(screen.getText().toString());
                     operation = b.getText().toString();
-                    screen.setText("0 ");
+                    isOperatorClicked = true;
                 } catch (NumberFormatException e) {
                     screen.setText("Error");
                 }
